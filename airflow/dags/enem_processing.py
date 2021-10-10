afd61ea7c67cc3d2da5f79e17773a54f81b2d03b
gLeitao <geovani.leitao@gmail.com>
2021-10-10 13:14:27 -0300

    ajustando a dag

--- airflow/dags/enem_processing.py
+++ airflow/dags/enem_processing.py
@@ -16,7 +16,7 @@ s3client = boto3.client("s3", aws_access_key_id=aws_access_key_id,
 
 # Usando a novíssima Taskflow API
 default_args = {
-    'owner': 'Neylson Crepalde',
+    'owner': 'Geovani Leitao',
     "depends_on_past": False,
     "start_date": days_ago(2),
     "email": ["airflow@airflow.com"],
@@ -33,11 +33,11 @@ def pipeline_enem():
     @task
     def emr_process_enem_data():
         cluster_id = client.run_job_flow(
-            Name='EMR-Ney-IGTI',
+            Name='EMR-Geovani-IGTI',
             ServiceRole='EMR_DefaultRole',
             JobFlowRole='EMR_EC2_DefaultRole',
             VisibleToAllUsers=True,
-            LogUri='s3://datalake-ney-igti-edc-tf/emr-logs',
+            LogUri='s3://datalake-geovani-igti-edc-uc-tf/emr-logs',
             ReleaseLabel='emr-6.3.0',
             Instances={
                 'InstanceGroups': [
@@ -56,10 +56,10 @@ def pipeline_enem():
                         'InstanceCount': 1,
                     }
                 ],
-                'Ec2KeyName': 'ney-igti-teste',
+                'Ec2KeyName': 'geovani-igti-teste',
                 'KeepJobFlowAliveWhenNoSteps': True,
                 'TerminationProtected': False,
-                'Ec2SubnetId': 'subnet-1df20360'
+                'Ec2SubnetId': 'subnet-0b60abafc70795b2c'
             },
 
             Applications=[{'Name': 'Spark'}],
@@ -109,7 +109,7 @@ def pipeline_enem():
                             '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
                             '--master', 'yarn',
                             '--deploy-mode', 'cluster',
-                            's3://datalake-ney-igti-edc-tf/emr-code/pyspark/01_delta_spark_insert.py'
+                            's3://datalake-geovani-igti-edc-uc-tf/emr-code/pyspark/01_delta_spark_insert.py'
                         ]
                 }
             }],
@@ -151,7 +151,7 @@ def pipeline_enem():
                                 '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
                                 '--master', 'yarn',
                                 '--deploy-mode', 'cluster',
-                                's3://datalake-ney-igti-edc-tf/emr-code/pyspark/02_delta_spark_upsert.py'
+                                's3://datalake-geovani-igti-edc-uc-tf/emr-code/pyspark/02_delta_spark_upsert.py'
                             ]
                     }
                 }]
