90f0a6e3847cb90798e5e363f54826b16ad63ab3
gLeitao <geovani.leitao@gmail.com>
2021-10-10 11:36:54 -0300

    sobe infra airflow

---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "key_pair_name"{
+  default = "geovani-igti-teste"
+}
+
+variable "airflow_subnet_id"{
+  default = "subnet-0b60abafc70795b2c"
+}
+
+variable "vpc_id"{
+  default = "vpc-0f39582eb30c12c4c"
+}
